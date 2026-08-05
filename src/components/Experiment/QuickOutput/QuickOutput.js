6ee82f9b9b98311207f0b04fdf6b77bff936f804
Changjae Lee <cjlee7128@users.noreply.github.com>
2026-08-05 13:55:15 -0400

    Add GPT-2 text explanation UI

--- src/components/Experiment/QuickOutput/QuickOutput.js
+++ src/components/Experiment/QuickOutput/QuickOutput.js
@@ -58,6 +58,7 @@ import AudioToAudioOutput from "./Outputs/AudioToAudio/AudioToAudioOutput";
 import VideoClassificationOutput from "./Outputs/VideoClassification/VideoClassificationOutput";
 import TableEditingOutput from "./Outputs/TableEditing/TableEditingOutput";
 import ExplanationPanel from "./Outputs/Classification/ExplanationPanel";
+import TextExplanationPanel from "./Outputs/Text/TextExplanationPanel";
 
 
 const defaultProps = {
@@ -75,8 +76,11 @@ export default function QuickOutput(givenProps) {
   const explanation = props.trialOutput?.results?.explanation;
   const showExplanation =
     !props.processFailed &&
-    props.trialOutput?.model?.output?.type === image_classification &&
+    [image_classification, textToText].includes(props.trialOutput?.model?.output?.type) &&
     explanation;
+  const explanationPanel = props.trialOutput?.model?.output?.type === textToText
+    ? <TextExplanationPanel explanation={explanation} />
+    : <ExplanationPanel explanation={explanation} />;
 
   const preview = props?.trialOutput?.inputs.length > 1 ? <MultiInputPreview inputs={props.trialOutput.inputs} onBackClicked={props.onBackClicked} /> : (
     <InputPreview
@@ -149,6 +153,7 @@ export default function QuickOutput(givenProps) {
           return (
             <TextOutput
               onBackClicked={props.onBackClicked}
+              onSubmit={props.runTrial}
               trial={props.trialOutput}
             />
           );
@@ -307,7 +312,7 @@ export default function QuickOutput(givenProps) {
       <div className={getElement("content")}>{makeOutput()}</div>
       {showExplanation && (
         <div className={getElement("explanation")}>
-          <ExplanationPanel explanation={explanation} />
+          {explanationPanel}
         </div>
       )}
       <div className={getElement("footer")}>
